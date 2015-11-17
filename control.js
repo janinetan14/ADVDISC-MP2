2febb25ef81a03bce36ecc457f1573013080a93c
raisagennellee <raisagennel_lee@yahoo.com>
2015-11-17 15:40:06 +0800

    added ui for playfair and adfcgv ciphers added control to adfcgv cipher

--- control.js
+++ control.js
@@ -15,12 +15,37 @@ var lastToggled;
 	});
 	
 	$(".btn").click(function(){
-		if (this.name == "hill-encrypt"){
+		if(this.name == "adfgvx-encrypt"){
+			var input = getADFGVXValue();
+			
+		}
+		else if (this.name == "adfgvx-decrypt"){
+			var input = getADFGVXValue();
+			
+		}
+		else if (this.name == "hill-encrypt"){
 			getHillValue();
 		}
+		else if (this.name == "hill-decrypt"){
+		}
+		else if (this.name == "playfair-encrypt"){
+		}
+		else if (this.name == "playfair-decrypt"){
+		}
+		
 	});
 })();
 
 function getHillValue(){
 	console.log($("textarea").val());
+	//console.log({matrix: matrix});
+}
+
+function getPlayfailValue(){
+}
+
+function getADFGVXValue(){
+	var message = $("#adfgvx-message").val();
+	var keyword = $("#adfgvx-key").val();
+	return {message: message, keyword: keyword};
 }
